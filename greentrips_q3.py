d4b8e4b7436e938a4412f970c4b1f81c0eee5d05
JadeYoung16 <98503520+JadeYoung16@users.noreply.github.com>
2026-03-20 11:14:14 -0400

    Add files via upload

---
+++ greentrips_q3.py
@@ -0,0 +1,76 @@
+import os 
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, StreamTableEnvironment
+
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            PULocationID INTEGER,
+            lpep_pickup_datetime BIGINT,
+            total_amount DOUBLE,
+            event_timestamp AS TO_TIMESTAMP_LTZ(lpep_pickup_datetime, 3), 
+            WATERMARK FOR event_timestamp AS event_timestamp - INTERVAL '5' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda:29092',
+            'topic' = 'green-trips',
+            'scan.startup.mode' = 'earliest-offset',
+            'properties.auto.offset.reset' = 'earliest',
+            'format' = 'json'
+        );
+    """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def create_hw7_sink_postgres(t_env):
+    table_name = 'green_trips_window_4'
+    sink_ddl = f"""
+        CREATE TABLE sink_table (
+            window_start TIMESTAMP(3),
+            PULocationID INT,
+            num_trips BIGINT,
+            PRIMARY KEY (window_start, PULocationID) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+    """
+    t_env.execute_sql(sink_ddl)
+    return 'sink_table'
+
+
+def window_job():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    source_table = create_events_source_kafka(t_env)
+    sink_table = create_hw7_sink_postgres(t_env)
+
+    try:
+        t_env.execute_sql(f"""
+        INSERT INTO {sink_table}
+        SELECT
+            window_start,
+            PULocationID,
+            COUNT(*) AS num_trips
+        FROM TABLE(
+            TUMBLE(TABLE {source_table}, DESCRIPTOR(event_timestamp), INTERVAL '5' MINUTES)
+        )
+        GROUP BY window_start, PULocationID;
+
+        """).wait()
+    except Exception as e:
+        print(f"Flink Job Failed: {str(e)}")
+
+if __name__ == '__main__':
+    window_job()
